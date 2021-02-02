48784945c524ea358dd8507c48640c2b460b31a3
diogosousa98 <becadicasousa@gmail.com>
2021-02-02 17:56:16 +0000

    act

--- homeBay/app.js
+++ homeBay/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 
 var imovelRoute = require('./routes/imovelRoute');
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/API/imoveis', imovelRoute);
 app.use('/API/clientes', clienteRoute);
